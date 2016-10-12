fd674fa656f5976e6537c030000e61962d9c27de
Alisher Satybaldin <Alisher737@gmail.com>
2016-10-12 14:19:35 +0800

    webpage

---
+++ js/default.js
@@ -0,0 +1,33 @@
+// Load Custom Google Font
+WebFont.load({
+    google: {
+        families: ['Ubuntu:300,400,500,700', 'Montserrat:400,700']
+    }
+});
+
+// Swipe Controls for Carousel
+$('.carousel').swiperight(function () {
+    $(this).carousel('prev');
+});
+$('.carousel').swipeleft(function () {
+    $(this).carousel('next');
+});
+
+// Google Map
+function googleMap() {
+    var map = document.getElementById('google-map');
+
+    var map_options = {
+            center: new google.maps.LatLng(3.0008, 101.5965),
+            scrollwheel: false,
+            zoom: 18,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+    var map = new google.maps.Map(map, map_options);
+
+  // Map Marker
+    var myLatlng = new google.maps.LatLng(3.00077, 101.596355);
+    var marker = new google.maps.Marker({position: myLatlng, map: map, icon: 'img/location-pin.svg'
+        });
+}
+google.maps.event.addDomListener(window, 'load', googleMap);
